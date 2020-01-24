1b2c2b7ad627e3140e57cf740395b5c4d3da9c0f
VKavun <vitaliy.kavun.1987@gmail.com>
2020-01-24 16:21:37 +0200

    Добавил отправку PHP

--- action.php
+++ action.php
@@ -1,48 +1,22 @@
 <?php
 	$msg_box = ""; // в этой переменной будем хранить сообщения формы
-	$errors = array(); // контейнер для ошибок
-	// проверяем корректность полей
-	if($_POST['user_name'] == "") 	 $errors[] = "Поле 'Ваше имя' не заполнено!";
-	if($_POST['user_email'] == "") 	 $errors[] = "Поле 'Ваш e-mail' не заполнено!";
-	if($_POST['text_comment'] == "") $errors[] = "Поле 'Текст сообщения' не заполнено!";
 
-	// если форма без ошибок
-	if(empty($errors)){		
-		// собираем данные из формы
-		$message  = "Имя пользователя: " . $_POST['user_name'] . "<br/>";
-		$message .= "E-mail пользователя: " . $_POST['user_email'] . "<br/>";
-		$message .= "Текст письма: " . $_POST['text_comment'];		
-		send_mail($message); // отправим письмо
-		// выведем сообщение об успехе
-		$msg_box = "<span style='color: green;'>Сообщение успешно отправлено!</span>";
-	}else{
-		// если были ошибки, то выводим их
-		$msg_box = "";
-		foreach($errors as $one_error){
-			$msg_box .= "<span style='color: red;'>$one_error</span><br/>";
-		}
-	}
+$to = "info@mb-ware.com"; // емайл получателя данных из формы
+$tema = "Форма обратной связи на PHP"; // тема полученного емайла
+$message = "Ваше имя: ".$_POST['user_name']."<br>";//присвоить переменной значение, полученное из формы name=name
+  $message .= "E-mail: ".$_POST['user_email']."<br>"; //полученное из формы name=email
+//$message .= "Номер телефона: ".$_POST['phone']."<br>"; //полученное из формы name=phone
+$message .= "Сообщение: ".$_POST['text_comment']."<br>"; //полученное из формы name=message
+$headers  = 'MIME-Version: 1.0' . "\r\n"; // заголовок соответствует формату плюс символ перевода строки
+  $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n"; // указывает на тип посылаемого контента
+mail($to, $tema, $message, $headers); //отправляет получателю на емайл значения переменных
+
+$msg_box = "Отпрввлено ок!";
 
 	// делаем ответ на клиентскую часть в формате JSON
 	echo json_encode(array(
 		'result' => $msg_box
 	));
 	
-	
-	// функция отправки письма
-	function send_mail($message){
-		// почта, на которую придет письмо
-		$mail_to = "maximuskavun@gmail.com"; 
-		// тема письма
-		$subject = "Письмо с обратной связи";
-		
-		// заголовок письма
-		$headers= "MIME-Version: 1.0\r\n";
-		$headers .= "Content-type: text/html; charset=utf-8\r\n"; // кодировка письма
-		$headers .= "From: Тестовое письмо <no-reply@test.com>\r\n"; // от кого письмо
-		
-		// отправляем письмо 
-		mail($mail_to, $subject, $message, $headers);
-	}
 	
 ?>
