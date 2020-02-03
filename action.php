23af60f11c5d71e3cbb7d8b7734af77ccf485fb9
VKavun <vitaliy.kavun.1987@gmail.com>
2020-02-03 21:32:31 +0200

    Убрал коментарии

--- action.php
+++ action.php
@@ -1,17 +1,16 @@
 <?php
 	$msg_box = ""; // в этой переменной будем хранить сообщения формы
 
-$to = "info@mb-ware.com"; // емайл получателя данных из формы
-$tema = "Форма обратной связи на PHP"; // тема полученного емайла
-$message = "Ваше имя: ".$_POST['user_name']."<br>";//присвоить переменной значение, полученное из формы name=name
-  $message .= "E-mail: ".$_POST['user_email']."<br>"; //полученное из формы name=email
-//$message .= "Номер телефона: ".$_POST['phone']."<br>"; //полученное из формы name=phone
-$message .= "Сообщение: ".$_POST['text_comment']."<br>"; //полученное из формы name=message
-$headers  = 'MIME-Version: 1.0' . "\r\n"; // заголовок соответствует формату плюс символ перевода строки
-  $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n"; // указывает на тип посылаемого контента
-mail($to, $tema, $message, $headers); //отправляет получателю на емайл значения переменных
+    $to = "info@mb-ware.com"; // емайл получателя данных из формы
+    $tema = "Форма обратной связи с MB-WARE"; // тема полученного емайла
+    $message = "Ваше имя: ".$_POST['user_name']."<br>";//присвоить переменной значение, полученное из формы name=name
+    $message .= "E-mail: ".$_POST['user_email']."<br>"; //полученное из формы name=email
+    $message .= "Сообщение: ".$_POST['text_comment']."<br>"; //полученное из формы name=message
+    $headers  = 'MIME-Version: 1.0' . "\r\n"; // заголовок соответствует формату плюс символ перевода строки
+    $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n"; // указывает на тип посылаемого контента
+    mail($to, $tema, $message, $headers); //отправляет получателю на емайл значения переменных
 
-$msg_box = "Отпрввлено ок!";
+    $msg_box = "Отправлено!";
 
 	// делаем ответ на клиентскую часть в формате JSON
 	echo json_encode(array(
